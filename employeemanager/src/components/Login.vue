b8d411f07b6aeea48571215a4e1429a1a3dda3a1
zuber <zuber1077@yahoo.com>
2018-10-15 10:15:36 +0800

    make employeemanager web app using vue.js

---
+++ employeemanager/src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m8 offset-m2">
+                    <div class="login card-panel green white-text center">
+                        <h3>Login</h3>
+                        <form>
+                            <div class="input-field">
+                                <div class="material-icons prefix">email</div>
+                                <input type="text" id="email" v-model="email">
+                                <label class="white-text" for="email">Email</label>
+                            </div>
+                            
+                            <div class="input-field">
+                                <div class="material-icons prefix">lock</div>
+                                <input type="password" id="password" v-model="password">
+                                <label class="white-text" for="password">Password</label>
+                            </div>
+                            <button v-on:click="login" class="btn btn-large grey lighten-4 black-text">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</template>
+
+
+<script>
+import firebase from 'firebase'
+export default {
+    name: 'login',
+    data: function() {
+    return {
+        email: '',
+        password: ''
+    }
+},
+methods: {
+    login: function (e) {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+            .then(user => {
+                alert(`You are logged in as ${user.email}`);
+                this.$router.push('/');
+            },
+             err => {  alert(err.message) }
+            ),
+        e.preventDefault();
+    }
+}
+}
+</script>
---
+++ employeemanager/src/components/Register.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="register">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m8 offset-m2">
+                    <div class="login card-panel lighten-4 black-text center">
+                        <h3>Register</h3>
+                        <form>
+                            <div class="input-field">
+                                <div class="material-icons prefix">email</div>
+                                <input type="text" id="email" v-model="email">
+                                <label for="email">Email</label>
+                            </div>
+                            
+                            <div class="input-field">
+                                <div class="material-icons prefix">lock</div>
+                                <input type="password" id="password" v-model="password">
+                                <label for="password">Password</label>
+                            </div>
+                            <button v-on:click="register" class="btn btn-large grey lighten-4 black-text">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</template>
+
+
+<script>
+import firebase from 'firebase'
+export default {
+    name: 'register',
+    data: function() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        register: function (e) {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+                .then(user => {
+                    alert(`Account created for ${user.email}`);
+                    this.$router.push('/');
+                }, err => { alert(err.message)} ),
+                
+            e.preventDefault();
+        }
+    }
+}
+</script>
